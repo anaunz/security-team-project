ef3ebb4d503060e421e890a8d87ccc4e96703b67
anaunz <superchok@localhost.localdomain>
2019-11-28 05:01:28 +0700

    another one

--- features/who/source/low.php
+++ features/who/source/low.php
@@ -6,7 +6,6 @@ header ("X-XSS-Protection: 0");
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Get input
 	$name = str_replace( '<script>', 'sCRiPt!?!!', strtolower($_GET[ 'name' ]) );
-	$name = str_replace( 'php', '', strtolower($name) );
 	
 	// Feedback for end user
 	$html .= '<pre>Hello, Po! So, this time thou are going with the name, ' . $name . '?</pre>';
